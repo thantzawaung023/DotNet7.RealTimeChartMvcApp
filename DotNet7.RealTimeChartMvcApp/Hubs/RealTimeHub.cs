88b40a20342faefe45eb8f4662b38250c41d01cf
thantzawaung023 <thantzinaung023@gmail.com>
2024-05-20 17:34:51 +0630

    Add project files.

---
+++ DotNet7.RealTimeChartMvcApp/Hubs/RealTimeHub.cs
@@ -0,0 +1,27 @@
+﻿using DotNet7.RealTimeChartMvcApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNet7.RealTimeChartMvcApp.Hubs
+{
+    public class RealTimeHub : Hub
+    {
+
+        public async Task SeverReceiveMessage()
+        {
+            Random random = new Random();
+            PieChartResponseModel modle = new PieChartResponseModel()
+            {
+                Data = new PieChartModel
+                {
+                    Labels = new List<string>{"A","B","C","D","E"},
+                    Series = new List<int> { random.Next(1,50), random.Next(1, 50), random.Next(1, 50), random.Next(1, 50), random.Next(1, 50) }
+                }
+            };
+            var count = 0;
+            await Clients.All.SendAsync("ClientReceiveMessage",JsonConvert.SerializeObject(modle));
+        }
+
+    }
+}
---
+++ DotNet7.RealTimeChartMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet7.RealTimeChartMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
